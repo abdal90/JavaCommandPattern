0bdb296d790a810c75326b5c41164bd918b465e5
abdal90 <Aabdalla3@mail.greenriver.edu>
2019-06-03 02:01:53 -0700

    part 2 left at 35:00

--- src/test_enrollments/RegistrationsTest.java
+++ src/test_enrollments/RegistrationsTest.java
@@ -1,15 +1,117 @@
 package test_enrollments;
 
+import commands.CommandType;
+import commands.ICommand;
+import commands.RegisterCommand;
 import registrations.GRClass;
 import registrations.Student;
 
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.concurrent.LinkedBlockingDeque;
+
 public class RegistrationsTest {
 
+    private static Queue<ICommand> commandsQueue = new LinkedBlockingDeque<>();
+
+    private static  Scanner console = new Scanner(System.in);
+
+    private static Student[] students;
+    private static GRClass[] classes;
     public static void main(String[] args) {
         // get some classes and students to test with
-        Student[] students = getStudents();
-        GRClass[] classes = getClasses();
+        students = getStudents();
+        classes = getClasses();
+
         // present a menu to the user to give them actions to perfrom
+        printMenuAndChoose();
+
+        console.close();
+    }
+
+    public static void printMenuAndChoose(){
+
+        while (true){
+            printMenu();
+            makeUserChoice();
+        }
+
+    }
+
+    public static void printMenu(){
+        // show menu options
+
+        CommandType[] types = CommandType.values()
+        for (int i = 0; i < types.length; i++) {
+            System.out.println((i + 1) + ", " + types[i].toString());
+        }
+        // execute menu options
+        System.out.println("x: execute all commands in the command queue");
+        // print out as "undo" option
+
+    }
+
+    public static void makeUserChoice(){
+        // perform the user's choice
+        String choice = console.nextLine();
+        switch (choice){
+            case "1":
+                registerStudent();
+                break;
+            case "2":
+                dropStudent();
+                break;
+            case "x":
+                executeQueueCommand();
+                break;
+
+        }
+    }
+
+    public static void executeQueueCommand(){
+        while (!commandsQueue.isEmpty()
+             ) {
+            ICommand command = commandsQueue.remove();
+            command.execute();
+
+            // keep track of the executed command
+            // can undo later
+        }
+    }
+
+    public static void registerStudent(){
+        ICommand register = new RegisterCommand(
+                getStudentFromUSer(), getClassFromUser());
+        commandsQueue.add(register);
+    }
+
+    public static void dropStudent(){
+        ICommand drop = new RegisterCommand(
+                getStudentFromUSer(), getClassFromUser());
+        commandsQueue.add(drop);
+    }
+
+    public static Student getStudentFromUSer(){
+        System.out.println("Enter a student name: ");
+        String studentName = console.nextLine();
+        for (int i = 0; i < students.length; i++) {
+            if (students[i].getName().equals(studentName)){
+                return students[i];
+            }
+        }
+        throw new IllegalStateException("Error finding student: " + studentName);
+    }
+
+    public static GRClass getClassFromUser(){
+        System.out.println("Enter a class name: ");
+        String className = console.nextLine();
+        for (int i = 0; i < classes.length; i++) {
+            if (classes[i].getName().equals(className)){
+                return classes[i];
+            }
+        }
+        throw new IllegalStateException("Error finding classes: " + className);
+
     }
 
     public static GRClass[] getClasses(){
